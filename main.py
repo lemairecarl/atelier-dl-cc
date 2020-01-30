15826c5b96fd1725516be05c832e59c494314c77
lemairecarl <c.lemaire@live.ca>
2020-01-30 17:09:15 -0500

    Use resnet50

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import argparse
 import time
 
 import torch
-from torch.utils.data import DataLoader
-import torchvision.transforms as transforms
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.models import resnet50
 
 from tinyimagenet import TinyImageNet, TinyImageNetVal
 from utils import accuracy
@@ -41,32 +41,7 @@ valid_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=False, 
 print('Train set size:', len(train_set))
 print('Num batches:', len(train_loader))
 
-
-class Net(nn.Module):
-    def __init__(self, num_classes):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)  # 64 -> 60
-        self.pool = nn.MaxPool2d(2, 2)  # 60 -> 30
-        self.conv2 = nn.Conv2d(6, 16, 5)  # 30 -> 26
-        # self.pool = nn.MaxPool2d(2, 2)  # 26 -> 13
-        self.conv3 = nn.Conv2d(16, 16, 5)  # 13 -> 9
-        self.fm_volume = 16 * 9 * 9  # feature map volume
-        self.fc1 = nn.Linear(self.fm_volume, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, num_classes)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = F.relu(self.conv3(x))
-        x = x.view(-1, self.fm_volume)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-
-net = Net(num_classes=len(train_set.classes)).to(device)
+net = resnet50(num_classes=len(train_set.classes)).to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9)
 
